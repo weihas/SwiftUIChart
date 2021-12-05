1c947437f0d1fc1c545059d61c6d1c0b64a617d7
Weiha Wang <weihas@icloud.com>
2021-12-05 13:53:45 +0800

    Initial Commit

---
+++ SwiftUIChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChart
+//
+//  Created by WeIHa'S on 2021/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIChart/SwiftUIChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartApp.swift
+//  SwiftUIChart
+//
+//  Created by WeIHa'S on 2021/12/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
